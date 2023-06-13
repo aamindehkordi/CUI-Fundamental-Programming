2f441efe323619d3a8ed1a81347ec2de64417d96
Ali Amin <aamindehkordi@gmail.com>
2023-06-13 00:28:27 -0700

    class hw import

---
+++ HW/Planets/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planets
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Planet p1 = new Planet("Earth", 5.97e24, 6.38e6);
+            double w1 = p1.GetWeight(90);
+            Console.WriteLine($"{p1},\n{w1} ");
+        }
+    }
+}
